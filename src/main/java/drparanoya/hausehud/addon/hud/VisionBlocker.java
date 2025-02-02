d20fbd4c0b1dbcc85355fb024235eb5bd0108b73
DrParanoya <158059657+DrParanoya@users.noreply.github.com>
2025-02-02 12:33:48 +0100

    update

--- src/main/java/drparanoya/hausehud/addon/hud/VisionBlocker.java
+++ src/main/java/drparanoya/hausehud/addon/hud/VisionBlocker.java
@@ -3,7 +3,6 @@ package drparanoya.hausehud.addon.hud;
 import drparanoya.hausehud.addon.HauseHUD;
 import meteordevelopment.meteorclient.renderer.GL;
 import meteordevelopment.meteorclient.renderer.Renderer2D;
-import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
@@ -11,7 +10,6 @@ import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.settings.ColorSetting;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
@@ -21,7 +19,7 @@ public class VisionBlocker extends HudElement {
 
     private final Setting<Double> scale = sgGeneral.add(new DoubleSetting.Builder()
         .name("Scale")
-        .description("How much of your vision should we block?")
+        .description("Scale of image.png")
         .defaultValue(1)
         .min(0)
         .sliderRange(0, 10)
@@ -29,8 +27,26 @@ public class VisionBlocker extends HudElement {
     );
     private final Setting<SettingColor> imageColor = sgGeneral.add(new ColorSetting.Builder()
         .name("Color")
-        .description("Color of the image.")
+        .description("Color of image.png.")
         .defaultValue(new SettingColor())
+        .build()
+    );
+    private final Setting<Double> width = sgGeneral.add(new DoubleSetting.Builder()
+        .name("Width")
+        .description("Width of image.png")
+        .defaultValue(255)
+        .min(0)
+        .noSlider()
+        .decimalPlaces(0)
+        .build()
+    );
+    private final Setting<Double> height = sgGeneral.add(new DoubleSetting.Builder()
+        .name("Height")
+        .description("Height of image.png")
+        .defaultValue(255)
+        .min(0)
+        .noSlider()
+        .decimalPlaces(0)
         .build()
     );
 
@@ -43,11 +59,11 @@ public class VisionBlocker extends HudElement {
 
     @Override
     public void render(HudRenderer renderer) {
-        setSize(scale.get() * 255, scale.get() * 255);
+        setSize(width.get() * scale.get(), height.get() * scale.get());
         MatrixStack matrixStack = new MatrixStack();
         GL.bindTexture(image);
         Renderer2D.TEXTURE.begin();
-        Renderer2D.TEXTURE.texQuad(this.x, this.y, scale.get() * 255, scale.get() * 255, imageColor.get());
+        Renderer2D.TEXTURE.texQuad(this.x, this.y, width.get() * scale.get(), height.get() * scale.get(), imageColor.get());
         Renderer2D.TEXTURE.render(matrixStack);
     }
 }
